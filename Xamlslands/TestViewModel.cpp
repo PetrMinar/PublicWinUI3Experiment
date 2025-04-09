90a29c506288745b01e107be43515ff36f2d2008
Petr Minář <petr.minar@gmail.com>
2025-04-09 15:25:34 +0200

    TestViewModel

---
+++ Xamlslands/TestViewModel.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "TestViewModel.h"
+#if __has_include("TestViewModel.g.cpp")
+#include "TestViewModel.g.cpp"
+#endif
+
+namespace winrt::ZXamlIslands::implementation
+{
+    int32_t TestViewModel::MyProperty()
+    {
+        throw 0;
+    }
+
+    void TestViewModel::MyProperty(int32_t value)
+    {
+    }
+}
---
+++ Xamlslands/TestViewModel.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "TestViewModel.g.h"
+
+namespace winrt::ZXamlIslands::implementation
+{
+    struct TestViewModel : TestViewModelT<TestViewModel>
+    {
+        TestViewModel() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::ZXamlIslands::factory_implementation
+{
+    struct TestViewModel : TestViewModelT<TestViewModel, implementation::TestViewModel>
+    {
+    };
+}
